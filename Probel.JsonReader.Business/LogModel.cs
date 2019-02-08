52a24519e0ed07259064873f64f7de562c7d8aa2
jibedoubleve <jibedoubleve@users.noreply.github.com>
2019-02-08 11:51:52 +0100

    Dev 1.5.0 (#18)
    * Hot fixes
    * Allow to copy logs into the clipboard

--- Probel.JsonReader.Business/LogModel.cs
+++ Probel.JsonReader.Business/LogModel.cs
@@ -7,15 +7,10 @@ namespace Probel.JsonReader.Business
         #region Properties
 
         public string Exception { get; set; }
-
         public string Level { get; set; }
-
         public string Logger { get; set; }
-
         public string Message { get; set; }
-
         public int ThreadId { get; set; }
-
         public DateTime Time { get; set; }
 
         #endregion Properties
